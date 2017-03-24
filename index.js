c3f7857693e085d13e8cca4985a6d395575524e5
Steve Kane <kanesteven@gmail.com>
2017-03-23 21:27:50 -0500

    added new stone texture and tweaked lighting more

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ load({
   manifest: {
     diffuse: {
       type: 'image',
-      src: 'textures/brick-diffuse.jpg'
+      src: 'textures/stone_COLOR.png'
     },
     normal: {
       type: 'image',
-      src: 'textures/brick-normal.png'
+      src: 'textures/stone_NRM.png'
     } 
   },
   onDone: launch
@@ -61,6 +61,8 @@ function launch ({ normal, diffuse }) {
       #pragma glslify: to_linear = require(glsl-gamma/in)
       #pragma glslify: to_gamma = require(glsl-gamma/out)
       #pragma glslify: perturb_normal = require(glsl-perturb-normal)
+      #pragma glslify: blinn_phong_specular = require(glsl-specular-blinn-phong)
+      #pragma glslify: oren_nayar_diffuse = require(glsl-diffuse-oren-nayar)
       #pragma glslify: attenuation = require(./attenuation)
 
       uniform mat4 view;
@@ -69,36 +71,38 @@ function launch ({ normal, diffuse }) {
       uniform sampler2D u_diffuse;
       uniform sampler2D u_normal;
       uniform float u_shininess;
+      uniform float u_roughness;
+      uniform float u_albedo;
       uniform float u_tiling_factor;
 
       varying vec3 v_position;
       varying vec3 v_normal;
       varying vec2 v_tx_coord;
 
-      const float light_radius = .1;
-      const float light_falloff = 3.;
-      const float ambient_factor = .01;
-      const vec3 ambient_color = vec3(.95);
+      const float light_radius = 10.;
+      const float light_falloff = .1;
       const vec3 specular_color = vec3(1.0, 1.0, 1.0);
 
       void main() {
-        vec3 v_light = (view * vec4(u_light, 1)).xyz;
         vec2 tile_tx = v_tx_coord * u_tiling_factor;
-        vec3 eye_dir = normalize(v_position);
+
+        vec3 eye_dir = normalize(-v_position);
+
+        vec3 v_light = (view * vec4(u_light, 1)).xyz;
         vec3 light_vector = v_light - v_position;
         vec3 light_dir = normalize(light_vector);
-        vec3 diffuse_color = to_linear(texture2D(u_diffuse, tile_tx)).rgb;
-        vec3 normal = to_linear(texture2D(u_normal, tile_tx)).rgb * 2. - 1.;
-        vec3 adjusted_normal = perturb_normal(normal, v_normal, eye_dir, tile_tx);
-        vec3 half_dir = normalize(light_dir + eye_dir);
         float light_dist = length(light_vector);
-        float diffuse = max(dot(adjusted_normal, light_dir), 0.0);
-        float specular = pow(max(dot(half_dir, adjusted_normal), 0.0), u_shininess);
+
+        vec3 diffuse_color = to_linear(texture2D(u_diffuse, tile_tx)).rgb;
+        vec3 normal_map = to_linear(texture2D(u_normal, tile_tx)).rgb;
+        vec3 adjusted_normal = perturb_normal(normal_map, v_normal, eye_dir, v_tx_coord);
+
+        float diffuse_factor = oren_nayar_diffuse(light_dir, v_position, adjusted_normal, u_roughness, u_albedo);
+        float specular_factor = .2 * blinn_phong_specular(light_dir, eye_dir, adjusted_normal, u_shininess);
         float falloff = attenuation(light_radius, light_falloff, light_dist);
 
-        gl_FragColor.rgb = ambient_color * ambient_factor;
-        gl_FragColor.rgb += diffuse * diffuse_color * falloff;
-        gl_FragColor.rgb += specular * specular_color * falloff;
+        gl_FragColor.rgb = diffuse_color * diffuse_factor * falloff;
+        gl_FragColor.rgb += specular_color * specular_factor * falloff;
         gl_FragColor = to_gamma(gl_FragColor);
         gl_FragColor.a = 1.;
       }
@@ -112,6 +116,8 @@ function launch ({ normal, diffuse }) {
       u_diffuse: regl.prop('geometry.diffuse'),
       u_normal: regl.prop('geometry.normal'),
       u_shininess: regl.prop('geometry.shininess'),
+      u_roughness: regl.prop('geometry.roughess'),
+      u_albedo: regl.prop('geometry.albedo'),
       u_tiling_factor: regl.prop('geometry.tiling_factor'),
       u_light: regl.prop('light'),
     },
@@ -146,22 +152,28 @@ function launch ({ normal, diffuse }) {
     diffuse: regl.texture({
       data: diffuse,
       wrapS: 'repeat',
-      wrapT: 'repeat'
+      wrapT: 'repeat',
+      mag: 'linear',
+      min: 'linear mipmap linear'
     }),
     normal: regl.texture({
       data: normal,
       wrapS: 'repeat',
-      wrapT: 'repeat'
+      wrapT: 'repeat',
+      mag: 'linear',
+      min: 'linear mipmap linear'
     }),
-    shininess: 800,
-    tiling_factor: 4,
+    shininess: 200,
+    albedo: .9,
+    roughess: 1, 
+    tiling_factor: 2,
     count: 6
   }
   var camera = Camera(regl, {
-    distance: 4,
+    distance: 3,
     theta: Math.PI / 2 // regl-camera default is ZY plane
   })
-  var light = [ 0, 1, 10 ]
+  var light = [ 0, 0, 1 ]
   var clearProps = { 
     color: [ 0, 0, 0, 1 ],
     depth: 1
@@ -174,10 +186,10 @@ function launch ({ normal, diffuse }) {
 
   window.wall = wall
   regl.frame(function ({ tick, time, viewportWidth, viewportHeight }) {
-    regl.clear(clearProps)
-    Vec3.copy(light, camera.eye)
-    console.log(light)
+    renderProps.light[0] = Math.sin(time)
+    renderProps.light[2] = Math.abs(Math.cos(time)) + 1
     renderProps.time = time
+    regl.clear(clearProps)
     camera(_ => render(renderProps))
   })
 }
